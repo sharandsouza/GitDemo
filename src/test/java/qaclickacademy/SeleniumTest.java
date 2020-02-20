d7e010c4b9946e2ecdaf907f5b5fffda44b72bfe
Sharan <sharandsouza87@gmail.com>
2020-02-20 11:17:42 +1100

    first commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void BrowserAUtomation()
+	{
+		System.out.println("BrowserAUtomation");	
+	}
+	@Test
+	public void elementsUi()
+	{
+		System.out.println("elementsUi");	
+	}
+}
